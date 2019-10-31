f05c3e449198e3c1c37104587b80c066a4ff06da
as12334 <kejunjie1991@163.com>
2019-11-01 02:40:05 +0800

    新增用户修改

--- WebContent/company/js/user/Add.js
+++ WebContent/company/js/user/Add.js
@@ -3,7 +3,7 @@ define(['common/BaseEditPage', 'bootstrapswitch'], function (BaseEditPage) {
     return BaseEditPage.extend({
         init: function () {
             this.formSelector = "#editUserForm";
-            // this._super(this.formSelector);
+            this._super(this.formSelector);
             this.bindEvent();
             this.onPageLoad();
         },
@@ -11,103 +11,21 @@ define(['common/BaseEditPage', 'bootstrapswitch'], function (BaseEditPage) {
          * 当前对象事件初始化
          */
         bindEvent: function () {
-            // this._super();
+            this._super();
             var _this = this;
-            $(_this.formSelector).on("click",".myLayerOk",function () {
-                var data_json;
-                var shareCredits = $("#shareCredits").text();
-                var shareStintOccupy = $("#superStintOccupy").text();
-                var a, reg, mesg, value, data = [];
-                $("#add-user").find("input").each(function () {
-                    if ($(this).attr("type") == "radio" && $(this).attr("checked")) {
-                        data.push($(this).attr("name") + ":" + $(this).attr("data-value"));
-                    } else if ($(this).attr("type") == "text") {
-                        reg = eval($(this).attr("reg"));
-                        value = $(this).val();
-                        data.push($(this).attr("name") + ":" + value);
-                         if ($(this).attr("name") == "result.credits" && parseInt(value) > parseInt(shareCredits)) {
-                            _this.myTips({ content: "上级可用餘額：" + shareCredits, obj: $(this), myclick: true });
-                            data = false;
-                            return false;
-                        // } else if ($(this).attr("name") == "result.credits" && parseInt(value) < data_json.maxCredits) {
-                        //     _this.myTips({ content: "可“回收”剩餘額度：" + data_json.maxCredits, obj: $(this), myclick: true });
-                        //     data = false;
-                        //     return false;
-                        } else if ($(this).attr("name") == "result.superiorOccupy" && parseInt(value) > parseInt(shareStintOccupy)) {
-                            _this.myTips({ content: "上級最高可設占成：" + shareStintOccupy + "%", obj: $(this), myclick: true });
-                            data = false;
-                            return false;
-                        // } else if ($(this).attr("name") == "result.stintOccupy" && value != "-1" && parseInt(value) < data_json.stintOccupyMin) {
-                        //     _this.myTips({ content: "下级已分配占成" + data_json.stintOccupyMin + "，可回收占成不可低于" + data_json.stintOccupyMin, obj: $(this), myclick: true });
-                        //     data = false;
-                        //     return false;
-                        } else if ($(this).attr("name") == "result.stintOccupy" && value != "-1" && parseInt(value) > parseInt($("span[name='shareSuperior']").text())) {
-                            _this.myTips({ content: "上级最高分配占成" + $("span[name='shareSuperior']").text() + "，限制占成不可高于" + $("span[name='shareSuperior']").text(), obj: $(this), myclick: true });
-                            data = false;
-                            return false;
-                        }
-                        else if (reg) {
-                            if (!reg.test(value)) {
-                                mesg = $(this).attr("mesg");
-                                if (mesg) { _this.myTips({ content: mesg, obj: $(this), myclick: true }); }
-                                data = false;
-                                return false;
-                            }
-                        }
-                    }
-                });
-                if(data){
-
-
-                    var value = $("input[name='result.username']").val();
-                    $.ajax({
-                        loading: true,
-                        url: root + "/remote/checkUsername.html",
-                        type: "post",
-                        dataType:"JSON",
-                        data:{"result.username":value,"result.userType":"","result.ownerId":""},
-
-                        success: function (data) {
-                            if(!data){
-                                _this.myTips({ content: "用戶名已存在！", obj: $(this), myclick: true });
-                            }else{
-                                var url = '/vSiteUser/saveManagerUser.html'
-                                $.ajax({
-                                    loading: true,
-                                    url: root + url,
-                                    type: "post",
-                                    dataType:"JSON",
-                                    data:$( _this.formSelector).serialize(),
-
-                                    success: function (data) {
-                                        alert(data.msg);
-                                        if(data.state){
-                                            $(".navListBox .onBtn").click();
-                                        }
-                                    },
-                                    error: function (data, state, msg) {
-                                        //超时导致后台返回,安全密码验证不做任何处理
-
-                                    }
-                                });
-                            }
-                        },
-                        error: function (data, state, msg) {
-
-                        }
-                    });
-                }
-
-            });
 
         },
-
         onPageLoad: function () {
+            this._super();
             var _this = this;
-
             $(_this.formSelector).on("change","select[name='set_water']",function () {
 
                 $("input[name='water']").val($(this).val());
+            });
+
+            $(_this.formSelector).on("click",".myLayerCancel",function () {
+
+                $(".navListBox .onBtn").click();
             });
 
             $(_this.formSelector).on("change","select[name='result.ownerId']",function () {
@@ -149,29 +67,26 @@ define(['common/BaseEditPage', 'bootstrapswitch'], function (BaseEditPage) {
             $("#shareCredits").text(superCredit);
 
         },
-        myTips: function (msg) {
-            var content = msg.content;
-            var elmOffset = msg.obj.offset();
-            var _top = msg.top || 3;
-            var _left = msg.left || 10;
-            var top = msg.top || elmOffset.top - _top; //控件top坐標
-            var left = msg.left || elmOffset.left + msg.obj.width() + _left; //控件left坐標
-            var myDiv = "<div id='myxTips' style='left:" + left + "px; top:" + top + "px;'><div id='myxTipsLeft'></div><div id='myxTipsContent'>" + content + "</div></div>";
-            $("#myxTips").remove();
-            $("body").append(myDiv);
-            if (msg.myclick) {
-                var count = 0;
-                $("body").unbind("click").click(function () {
-                    count++;
-                    if (count > 1) {
-                        $("#myxTips").remove();
-                        $("body").unbind("click");
-                    }
-                });
+
+
+
+        saveValid: function (e) {
+            var _this=this;
+            if(!this.validateForm(e)){
+                return false;
             }
-        },
-        saveCallbakUser : function () {
-            $(".navListBox .onBtn").click();
+            window.top.topPage.ajax({
+                url: root+"/vSiteUser/saveManagerUser.html",
+                async:false,
+                dataType:'json',
+                data:_this.getCurrentFormData(e),
+                success: function (data) {
+                   alert(data.msg);
+                    $(".navListBox .onBtn").click();
+                },
+                error: function (e) {
+                }
+            });
         }
     })
 });
