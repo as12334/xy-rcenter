83e36d539db307cc7be0aa7f93c0edf45c9dc9ad
as12334 <kejunjie1991@163.com>
2019-11-10 02:01:35 +0800

    赔率设置初始化

--- WebContent/company/js/lottery/cqssc.js
+++ WebContent/company/js/lottery/cqssc.js
@@ -19,25 +19,73 @@ define(['common/BaseEditPage'], function (BaseEditPage) {
         },
 
 
+        /**
+         * 保存赔率
+         * @param e
+         */
+        saveLotteryOdd: function (e, option) {
+            var siteId = $("#search_id").val();
+            if (!siteId) {
+                e.page.showPopover(e, option, 'danger', '站点id不能为空', true);
+                $(e.currentTarget).unlock();
+                return;
+            }
+            var code = $("#searchDiv a.ssc-active").attr("code");
+            var $form = $(this.getCurrentForm(e));
+            var $target = $(e.currentTarget);
+            var validate = $form.validate();
+            var array = [];
+            var group;
+            var oddObj;
+            var $input;
+            var odd;
+            var ori;
+            var obj;
+            var limit;
+            var data = {};
+            var rebateObj;
 
 
-        saveValid: function (e) {
-            var _this=this;
-            // if(!this.validateForm(e)){
-            //     return false;
-            // }
+            var code = $("#lotteryCode").val();
+            $(".betSortTr").each(function () {
+                var betSort = $(this).find("input[name='betSort']").val();
+                var oddA = $(this).find("input[name='oddA']").val();
+                var oddB = $(this).find("input[name='oddB']").val();
+                var oddC = $(this).find("input[name='oddC']").val();
+                var maxOdd = $(this).find("input[name='maxOdd']").val();
+                obj = {
+                    'code': code,
+                    'betSort': betSort,
+                    'oddA': oddA,
+                    'oddB': oddB,
+                    'oddC': oddC,
+                    'maxOdd': maxOdd
+                };
+                array.push(obj);
+            });
+            if (array.length <= 0) {
+                e.page.showPopover(e, option, 'success', '保存成功', true);
+                $target.unlock();
+                return;
+            }
+            var url = root + "/siteLotteryOdds/saveSiteLotteryOdds.html";
+            data['lotteryOddJson'] = JSON.stringify(array);
             window.top.topPage.ajax({
-                url: root+"/vSiteUser/saveManagerUser.html",
-                async:false,
-                dataType:'json',
-                data:_this.getCurrentFormData(e),
+                url: url,
+                data: data,
+                type: 'POST',
+                dataType: 'json',
                 success: function (data) {
-                    alert(data.msg);
-                    $(".navListBox .onBtn").click();
-                },
-                error: function (e) {
+                    if (data.state == true) {
+                        alert(data.msg)
+                        $target.unlock();
+                    } else {
+                        e.page.showPopover(e, option, 'danger', '保存失败', true);
+                    }
+                    $target.unlock();
                 }
             });
-        },
-    })
+        }
+
+    });
 });
