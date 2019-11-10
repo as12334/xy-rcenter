3f8f9fd5aeb24cff5b4b8a8e73e05c8b1a9044b1
as12334 <kejunjie1991@163.com>
2019-11-11 01:45:01 +0800

    重庆时时彩赔率设置

--- WebContent/company/js/lottery/cqssc.js
+++ WebContent/company/js/lottery/cqssc.js
@@ -10,9 +10,57 @@ define(['common/BaseEditPage'], function (BaseEditPage) {
         bindEvent: function () {
             this._super();
             var _this = this;
+            $(this.formSelector).on("change", "td input[type='checkbox']", function () {
+                if ($(this).is(':checked')) {
+                    $(this).parent().parent("tr").addClass("myqhs");
+                } else {
+                    $(this).parent().parent("tr").removeClass("myqhs");
+                }
+            });
 
 
+            $(this.formSelector).on("click","#od-set .odds-kj span",function () {
+                var value = parseFloat($(this).html());
+                var od_a = $(_this.formSelector).find("input[name='od-a']").is(':checked');
+                var od_b = $(_this.formSelector).find("input[name='od-b']").is(':checked');
+                var od_c =$(_this.formSelector).find("input[name='od-c']").is(':checked');
+                var od_max = $(_this.formSelector).find("input[name='od-max']").is(':checked');
+                var dow;
+                $(_this.formSelector).find("tbody tr.myqhs").each(function () {
+                    $(_this.formSelector).find("input[type='text']").each(function (i) {
+                        dow = value + parseFloat($(this).val());
+                        if (i == 0) {
+                            if (od_a && dow >= 0) {
+                                $(this).val(_this.forDight(dow, 4));
+                            } else if (od_a) {
+                                $(this).val("0");
+                            }
+                        } else if (i == 1) {
+                            if (od_b && dow >= 0) {
+                                $(this).val(_this.forDight(dow, 4));
+                            } else if (od_b) {
+                                $(this).val("0");
+                            }
+                        } else if (i == 2) {
+                            if (od_c && dow >= 0) {
+                                $(this).val(_this.forDight(dow, 4));
+                            } else if (od_c) {
+                                $(this).val("0");
+                            }
+                        } else if (i == 3) {
+                            if (od_max && dow >= 0) {
+                                $(this).val(_this.forDight(dow, 4));
+                            } else if (od_max) {
+                                $(this).val("0");
+                            }
+                        }
+                    });
+                });
+            });
         },
+
+            //快捷設置賠率
+
         onPageLoad: function () {
             this._super();
             var _this = this;
@@ -23,7 +71,7 @@ define(['common/BaseEditPage'], function (BaseEditPage) {
          * 保存赔率
          * @param e
          */
-        saveLotteryOdd: function (e, option) {
+        saveLotterysOdd: function (e, option) {
             var $target = $(e.currentTarget);
             var array = [];
             var obj;
@@ -71,6 +119,15 @@ define(['common/BaseEditPage'], function (BaseEditPage) {
                     }
                 }
             });
+        },
+        //重置選項
+        resetOption: function (e, option) {
+            $(this.formSelector).find("tr.myqhs").removeClass("myqhs");
+            $(this.formSelector).find("td input[type='checkbox']").attr("checked", false);
+        },
+        forDight: function (Dight, How) {
+            Dight = Math.round(Dight * Math.pow(10, How)) / Math.pow(10, How);
+            return parseFloat(Dight);
         }
 
     });
